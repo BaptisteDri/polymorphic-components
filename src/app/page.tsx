f5218394b9de2a0c7ad8586d3f8adab0b87dfba5
Baptiste Drillien <baptiste.drillien@live.fr>
2024-03-07 11:30:04 +0100

    feat: use cva and tailwind merge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,54 @@
-import { Button } from "@/ui/components/button";
-import { NextPage } from "next";
+import { Button } from "@/ui/design-system/button";
 import Link from "next/link";
 
-const Home: NextPage = () => {
+const Home = () => {
   return (
-    <main className="h-dvh grid place-items-center">
-      <div className="space-x-4">
-        <Button as={Link} href={"/test"}>
-          next link
-        </Button>
-        <Button as={"a"} href={"/test"}>
-          link
-        </Button>
-        <Button>button</Button>
+    <main className="h-dvh bg-gray-50 grid place-items-center">
+      <div className="space-y-10">
+        <section className="space-x-4 flex">
+          <Button as={Link} href={"/test"}>
+            internal link
+          </Button>
+          <Button
+            as={"a"}
+            href={"https://www.example.com"}
+            rel="noreferrer"
+            target="_blank"
+          >
+            external link
+          </Button>
+          <Button>button</Button>
+        </section>
+        <section className="space-x-4 flex">
+          <Button as={Link} href={"/test"} intent={"secondary"}>
+            internal link
+          </Button>
+          <Button
+            as={"a"}
+            href={"https://www.example.com"}
+            rel="noreferrer"
+            target="_blank"
+            intent={"secondary"}
+          >
+            external link
+          </Button>
+          <Button intent={"secondary"}>button</Button>
+        </section>
+        <section className="space-x-4 flex">
+          <Button as={Link} href={"/test"} intent={"ghost"}>
+            internal link
+          </Button>
+          <Button
+            as={"a"}
+            href={"https://www.example.com"}
+            rel="noreferrer"
+            target="_blank"
+            intent={"ghost"}
+          >
+            external link
+          </Button>
+          <Button intent={"ghost"}>button</Button>
+        </section>
       </div>
     </main>
   );
---
+++ src/ui/design-system/button.tsx
@@ -0,0 +1,60 @@
+// type PolymorphicProps<Element extends React.ElementType, Props> = Props &
+//   Omit<React.ComponentProps<Element>, "as"> & {
+//     as?: Element;
+//   };
+
+// type Props = {
+//   // custom props here
+// };
+
+// const defaultElement = "button";
+
+// export const Button = <
+//   Element extends React.ElementType = typeof defaultElement
+// >(
+//   props: PolymorphicProps<Element, Props>
+// ) => {
+//   const { as: Component = defaultElement, ...rest } = props;
+//   return <Component {...rest} />;
+// };
+
+import { twMerge } from "tailwind-merge";
+import { cva, type VariantProps } from "class-variance-authority";
+
+// Récupérer les types dynamiquement selon l'élément en retirant le as par défaut
+type PolymorphicProps<Element extends React.ElementType, Props> = Props &
+  Omit<React.ComponentProps<Element>, "as"> & {
+    as?: Element;
+  };
+
+// Ajouter du style au bouton
+const button = cva(["font-medium", "py-2.5", "px-3.5", "rounded-md"], {
+  variants: {
+    intent: {
+      primary: ["bg-blue-500", "text-white"],
+      secondary: ["bg-black", "text-white"],
+      ghost: ["text-blue-500"],
+    },
+  },
+  defaultVariants: {
+    intent: "primary",
+  },
+});
+
+// Récupérer les variants grâce à CVA, en l'occurence l'intent
+type Props = VariantProps<typeof button>;
+
+// Définir l'élément par défaut si as n'est pas renseigné
+const defaultElement = "button";
+
+// Composant Button correctement typé et stylisé
+export const Button = <
+  Element extends React.ElementType = typeof defaultElement
+>(
+  props: PolymorphicProps<Element, Props>
+) => {
+  const { as: Component = defaultElement, className, intent, ...rest } = props;
+  return (
+    <Component className={twMerge(button({ className, intent }))} {...rest} />
+  );
+};
